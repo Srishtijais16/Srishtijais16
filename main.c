780b7e6e6d9ed98b53b3c876e813803e31f02a5b
Srishtijais16 <152182634+Srishtijais16@users.noreply.github.com>
2024-07-20 13:23:26 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  binary search
+//
+//  Created by srishti   on 27/12/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
